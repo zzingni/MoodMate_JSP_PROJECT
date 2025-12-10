5b829e9a2289943871174167c561048e3a09a4f8
zzingni <wldms001313@gmail.com>
2025-12-11 03:49:25 +0900

    feat : 모임 멤버 관리를 위한 repository & service 생성

--- src/main/java/com/example/demo/meeting/MeetingService.java
+++ src/main/java/com/example/demo/meeting/MeetingService.java
@@ -2,16 +2,24 @@ package com.example.demo.meeting;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class MeetingService {
 	private final MeetingRepository meetingRepository;
+	private final UserRepository userRepository;
 	
 	public void insert(Meeting meeting) {
 		meetingRepository.save(meeting);
 	}
 	
+	public void joinMeeting(int meetingId, int userId) {
+		Meeting meeting = meetingRepository.findById(meetingId).orElseThrow();
+		User user = userRepository.findById(userId).orElseThrow();
+	}
 	
 }
--- src/main/java/com/example/demo/member/MemberController.java
+++ src/main/java/com/example/demo/member/MemberController.java
@@ -1,11 +1,39 @@
 package com.example.demo.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.meeting.MeetingRepository;
+import com.example.demo.meeting.MeetingService;
+import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/meeting")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
+    private final MeetingService meetingService;
+	private final UserRepository userRepository;
+	private final MeetingRepository meetingRepository;
+	
+	// 모임에 참가신청 
+	@GetMapping("/{id}/join")
+	public String joinMeeting(@PathVariable("meetingId") int meetingId,
+							HttpSession session) {
+		 User user = (User) session.getAttribute("loginUser");
+		 
+		 if (user == null) {
+		        return "redirect:/login";
+		    }
+		 
+		 meetingService.joinMeeting(meetingId, user.getUserId());
+		 
+		 return "redirect:/meeting/" + meetingId;
+	}
 }
