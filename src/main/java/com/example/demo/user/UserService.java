15aff04375b14b0c9981666b83e03883b7feadd2
zzingni <wldms001313@gmail.com>
2025-12-10 11:22:14 +0900

    feat : UserService 생성

---
+++ src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,17 @@
+package com.example.demo.user;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+	// 테이블 처리 전담
+	private final UserRepository userRepository;
+	
+	// 데이터 삽입
+	public void insert(User user) {
+		userRepository.save(user);
+	}
+}
